19997d18d962a42caae7daaf6e7a93d4e28a185c
shekhovtsovsergey <98221024+shekhovtsovsergey@users.noreply.github.com>
2023-02-08 22:48:49 +0300

    Lesson15.1

--- src/main/java/ru/otus/lesson15/domain/Book.java
+++ src/main/java/ru/otus/lesson15/domain/Book.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "books")
@@ -17,6 +18,7 @@ public class Book {
 
     private String name;
 
+    @DBRef
     private Author author;
 
     private Genre genre;
